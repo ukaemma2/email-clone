b619e29b27560ab966e2c820462745f37a8ad36c
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-19 01:09:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Let's Build gmail</h2>
+    </div>
+  );
+}
+
+export default App;
