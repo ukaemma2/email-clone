a3bb148480a7a35ce401e8ca79564cb4cc6fece3
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-21 01:43:50 +0100

    redux set up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 // import EmailIcon from '@material-ui/icons/Email';
-import MenuIcon from '@material-ui/icons/Menu';
+import { ArrowDropDown, Menu, Search} from '@material-ui/icons';
 import './Header.css'
 import { IconButton } from "@material-ui/core";
 export const Header = () => {
   return (
-    <div>
+    <div className="header">
      <div className="header_left">
         <IconButton>
-          <MenuIcon />
+          <Menu />
         </IconButton>
+        <img 
+          src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg" 
+          alt="gmail" 
+        />
      </div>
      <div className="header_middle">
-
-     </div>
+       <IconButton>
+          <Search />
+       </IconButton>
+       <input type="text" />
+       <IconButton>
+        <ArrowDropDown />
+       </IconButton>
+     </div> 
      <div className="header_right">
 
      </div>
