05660d9863a40d73f02d699b9d3d35efff67d4a8
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-22 01:04:13 +0100

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,34 @@
 import React from "react";
 // import EmailIcon from '@material-ui/icons/Email';
-import { ArrowDropDown, Menu, Search} from '@material-ui/icons';
+import { Apps, ArrowDropDown, Menu, Notifications, Search} from '@material-ui/icons';
 import './Header.css'
-import { IconButton } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 export const Header = () => {
   return (
     <div className="header">
-     <div className="header_left">
+     <section className="header_left">
         <IconButton>
-          <Menu />
+          <Menu className="menu" />
         </IconButton>
-        <img 
+        <img
           src="https://cdn.vox-cdn.com/thumbor/8fWz6qpiMYMsZhY4vrc9Vhl5yL8=/0x110:1320x770/fit-in/1200x600/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg" 
           alt="gmail" 
         />
-     </div>
-     <div className="header_middle">
-       <IconButton>
-          <Search />
-       </IconButton>
-       <input type="text" />
-       <IconButton>
+     </section>
+     <section className="header_middle">
+        <Search />
+        <input placeholder="Search mail" type="text" />
         <ArrowDropDown />
-       </IconButton>
-     </div> 
-     <div className="header_right">
-
-     </div>
+     </section> 
+     <section className="header_right">
+        <IconButton>
+          <Apps />
+        </IconButton>
+        <IconButton>
+          <Notifications />
+        </IconButton>
+        <Avatar className="avatar" />
+     </section>
     </div>
   )
 }
