28f304bc361825343440116eeff9b723885a1816
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-27 23:51:46 +0100

    header component

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,19 +1,21 @@
 
 import React from "react"
 import {Button} from '@material-ui/core'
-import {Add} from '@material-ui/icons'
+import {Add, Inbox} from '@material-ui/icons'
+
 import './Sidebar.css'
+import SidebarOption from "../sidebarOption/SidebarOption"
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <Button 
-        startIcon={<Add fontSize="large" 
-        className="sidebar_compose"/>}
+      <Button className="sidebar_compose"
+        startIcon={<Add fontSize="large"
+        />}
       >
         Compose
       </Button>
+      <SidebarOption Icon={Inbox} title="Inbox" number={54} />
     </div>
   )
 }
-
 export default Sidebar
