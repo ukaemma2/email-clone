b260d0759e63198dc917fff6c7e1e53a4af51241
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-11-30 22:59:24 +0100

    header component

--- src/components/sidebarOption/SidebarOption.jsx
+++ src/components/sidebarOption/SidebarOption.jsx
@@ -4,7 +4,7 @@ import "./SidebarOption.css"
 function SidebarOption(props) {
   const {Icon, title, number, selected} = props
   return (
-    <div className={`sidebarOption ${selected && 'sidebarOption--active'}`}>
+    <div className={` ${selected? 'sidebarOption--active' : 'sidebarOption'}`}>
       <Icon />
       <h3>{title}</h3>
       <p>{number}</p>
