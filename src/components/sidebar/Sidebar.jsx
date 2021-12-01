197dc6d0f49d17fbd6c193ae2ddea20f8fcc08dd
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-12-01 02:39:09 +0100

    react-router

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,19 @@
 
 import React from "react"
-import {Button} from '@material-ui/core'
-import {Add, Inbox, Star} from '@material-ui/icons'
+import {Button, IconButton} from '@material-ui/core'
+import {
+  AccessTime, 
+  Add, 
+  Duo, 
+  ExpandMore, 
+  Inbox, 
+  LabelImportant, 
+  NearMe, 
+  Note, 
+  Person, 
+  Phone, 
+  Star
+} from '@material-ui/icons'
 
 import './Sidebar.css'
 import SidebarOption from "../sidebarOption/SidebarOption"
@@ -14,8 +26,32 @@ const Sidebar = () => {
       >
         Compose
       </Button>
-      <SidebarOption Icon={Inbox} title="Inbox" number={54} />
-      <SidebarOption Icon={Star} title="Star" number={54} />
+      <SidebarOption 
+        Icon={Inbox} 
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={Star} title="Starred" number={74} />
+      <SidebarOption Icon={AccessTime} title="Snoozed" number={14} />
+      <SidebarOption Icon={LabelImportant} title="Important" number={20} />
+      <SidebarOption Icon={NearMe} title="Sent" number={37} />
+      <SidebarOption Icon={Note} title="Drafts" number={60} />
+      <SidebarOption Icon={ExpandMore} title="More" number={222} />
+      
+      <div className="sidebar_footer">
+        <div className="sidebar_footer_icons">
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/sidebarOption/SidebarOption.jsx
+++ src/components/sidebarOption/SidebarOption.jsx
@@ -4,11 +4,10 @@ import "./SidebarOption.css"
 function SidebarOption(props) {
   const {Icon, title, number, selected} = props
   return (
-    <div className={` ${selected? 'sidebarOption--active' : 'sidebarOption'}`}>
+    <div className={`sidebarOption ${selected && "sidebarOption--active" }`}>
       <Icon />
       <h3>{title}</h3>
       <p>{number}</p>
-      
     </div>
   )
 }
