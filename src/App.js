3a3d53c913fb59891493461ae066093b2a128efa
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-12-02 15:47:51 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+
 import React from 'react';
- 
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import './App.css';
 import { Header } from './components/header/Header';
+import EmailList from './components/mails/EmailList';
+import Mail from './components/mails/Mail';
 import Sidebar  from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-    <Header />
-    <Sidebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Sidebar />
+        <div className="app__body">
+          <Switch>
+            <Route path="/mail">
+              <Mail />
+            </Route>
+            <Route>
+              <EmailList />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/mails/EmailList.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const EmailList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EmailList
