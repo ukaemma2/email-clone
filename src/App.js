badbd7ab8424b01112a48fdcaf1e9e80cec63920
Uka Emmanuel Ogbonna <ukaemmanuel200@gmail.com>
2021-12-05 20:18:46 +0100

    react-router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 
-import {BrowserRouter, Routes,  Route} from 'react-router-dom'
+import {  BrowserRouter,  Routes,  Route} from "react-router-dom";
 import './App.css';
 import { Header } from './components/header/Header';
 import EmailList from './components/mails/EmailList';
@@ -15,19 +15,15 @@ function App() {
       <div className="App">
         <Header />
         <Sidebar />
+        <EmailList />
         <div className="app__body">
           <Routes>
-            <Route path="/Mail">
-              <Mail />
-            </Route>
-            <Route path="/EmailList"> 
-              <EmailList />
-            </Route>
+            <Route path="mail" element={<Mail />} />
+            <Route path="emailList" element={<EmailList />} /> 
           </Routes>
         </div>
       </div>
     </BrowserRouter>  
   );
 }
-
 export default App;
--- src/components/mails/EmailList.jsx
+++ src/components/mails/EmailList.jsx
@@ -1,9 +1,19 @@
+import { IconButton } from "@material-ui/core"
+import { ArrowDropDown, CheckBoxOutlineBlank } from "@material-ui/icons"
 import React from "react"
+
 
 const EmailList = () => {
   return (
     <div className="emailist">
-      
+      <div className="email_list_setting">
+        <div className="email_list_setting_left">
+          <CheckBoxOutlineBlank />
+          <IconButton>
+            <ArrowDropDown />
+          </IconButton>
+        </div>
+      </div>
     </div>
   )
 }
